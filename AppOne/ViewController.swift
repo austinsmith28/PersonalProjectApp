27f2e852463e6637d0b772efbac17272ea954544
austinsmith28 <flubahdub@gmail.com>
2019-03-04 20:27:27 -0500

    initial commit after cocoa pod and SnapChat SDK install plist is unchanged

---
+++ AppOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppOne
+//
+//  Created by David Lepore on 3/4/19.
+//  Copyright © 2019 David&Austin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+
+platform :ios, '9.0'
+
+target 'AppOne' do
+
+  use_frameworks!
+
+  pod 'SnapSDK'
+
+end
